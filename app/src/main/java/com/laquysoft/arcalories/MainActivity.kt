1c60bfb259c38cfd6d14d2df5ef74bc6c9125d36
joaobiriba <glaquidara@gmail.com>
2017-10-27 22:56:16 +0200

    Activate Firebase Cloud Storage

---
+++ app/src/main/java/com/laquysoft/arcalories/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.laquysoft.arcalories
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+
+class MainActivity : AppCompatActivity() {
+
+    internal var storage: FirebaseStorage?=null
+    internal var storageReference: StorageReference?=null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        storage = FirebaseStorage.getInstance()
+        storageReference = storage!!.getReference()
+    }
+}
